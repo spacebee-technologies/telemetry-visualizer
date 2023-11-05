505b0aa9e5a5ba2314bfb671afe2d240b30f5457
sanntin <sanntinunez@gmail.com>
2023-11-05 19:43:54 -0300

    add error:no telemery

--- app/telemetry_manager.py
+++ app/telemetry_manager.py
@@ -13,13 +13,19 @@ class TelemetryManager:
         for telemetry in self.telemetries:
             if telemetry.getOperationNumber() == id:
                 return telemetry
-        return id
+        return None
 
     def receiveTelemetries(self):
         pack_data=self.communication.receive()
-        unpack_data=self.message_manager.unpack(pack_data)
-        telemetry=self.getTelemetry(unpack_data['operation_id'])
-        data=telemetry.parseData(unpack_data['body'])
-        telemetry.loadParameters(data,unpack_data['transaction_id'],unpack_data['timestamp'],unpack_data['area_version'])
-        return telemetry
+        if pack_data:
+            unpack_data=self.message_manager.unpack(pack_data)
+            telemetry=self.getTelemetry(unpack_data['operation_id'])
+            
+            if telemetry:
+                data=telemetry.parseData(unpack_data['body'])
+                telemetry.loadParameters(data,unpack_data['transaction_id'],unpack_data['timestamp'],unpack_data['area_version'])
+                return telemetry
+            else:
+                print(f"ERRROR: telemerty id: {unpack_data['operation_id']} not found")
+                return None
     
